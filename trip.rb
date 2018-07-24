da13820e0acefdd44f32996afbc2fc1b17f60898
Ceora Ford <ceoraallthetime@gmail.com>
2018-07-24 17:39:28 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -13,4 +13,10 @@ stay = gets.chomp.capitalize  #don't forget to Capitilize the input
 
 # Lastly, puts it all back by interpolating these values in a string.
 
-puts ""
+puts "Great! What places would you like to visit? "
+places = gets.chomp.capitalize
+puts "Cool! What foods would you like to eat during your visit?"
+eat = gets.chomp
+puts "Great choices! How many nights would you like to stay?"
+nights = gets.chomp
+puts "It sounds like you'll have a great trip! You want to stay at #{stay} and visit #{places}. You also want to try #{eat}. You'll have a wonderful time during your #{nights} night stay!"
